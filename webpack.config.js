7ca2f0c507e6acfddea4a4d20f72228cde0ec326
Jack McCown <jmccown@cbinsights.com>
2017-09-18 14:10:18 -0400

    localize build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 const path = require('path');
 module.exports = {
   devtool: 'eval-cheap-module-source-map',
+  entry: path.resolve(__dirname, 'index.js'),
+  output: { filename: 'bundle.js' },
   module: {
     rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js']
+    extensions: ['.js'],
   },
-  target: 'node'
+  target: 'node',
 };
